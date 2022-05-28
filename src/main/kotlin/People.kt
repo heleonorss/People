85773c36ec17adb5cab935cd1d1ad1aae8f8ca49
heleonorss <heleonorss@yandex.ru>
2022-05-29 00:53:38 +0300

    edits

--- src/main/kotlin/People.kt
+++ src/main/kotlin/People.kt
@@ -1,10 +1,8 @@
 fun main() {
     while (true) {
         var likes = readLine()?.toUInt() ?: return
-        val likesSum = (likes - 1U)
+        val endingOfTheWord = if (likes == 11U || likes % 100U == 11U || likes % 10U != 1U) "людям" else "человеку"
+        println("Понравилось $likes $endingOfTheWord")
 
-        if (likesSum % 10U == 0U) {
-            print("Понравилось $likes человеку")
-        } else print("Понравилось $likes людям")
     }
     }
